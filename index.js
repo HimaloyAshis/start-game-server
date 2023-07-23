3c70eeaa3020100849334b6e530cbbc91632d513
himaloyash <himaloyashis98@gmail.com>
2023-07-23 13:00:37 +0600

    back-end setup done

--- index.js
+++ index.js
@@ -1,17 +1,51 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors')
 
 
 app.use(cors())
 app.use(express.json())
 
+// BookinColleges
+// tPVOtf6wi70QXjYZ
 
-app.get('/', (req, res)=>{
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6ogtg9l.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        // await client.connect();
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
     res.send('game start is live now')
 })
 
-app.get(port, ()=>{
-    app.listen(`Game start is in the back-end`)
+app.listen(port, () => {
+    console.log(`Game start is in the back-end port ${port}`)
 })
