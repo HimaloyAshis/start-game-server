e9dc13224ec51f0af94518d56f537780f31d762e
himaloyash <himaloyashis98@gmail.com>
2023-07-23 12:06:51 +0600

    back-end setup done

--- index.js
+++ index.js
@@ -6,3 +6,12 @@ const cors = require('cors')
 
 app.use(cors())
 app.use(express.json())
+
+
+app.get('/', (req, res)=>{
+    res.send('game start is live now')
+})
+
+app.get(port, ()=>{
+    app.listen(`Game start is in the back-end`)
+})
