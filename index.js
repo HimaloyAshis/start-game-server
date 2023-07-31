a5e27162e9f2aa2ca9d4ae36ea6aa9798bc77c3f
himaloyash <himaloyashis98@gmail.com>
2023-07-31 10:53:34 +0600

    ok

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require('cors')
 
 
@@ -28,10 +28,30 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         const bookingCollection = client.db('bookingColleges').collection('colleges')
+        const reviewCollection = client.db('bookingColleges').collection('reviews')
 
 
-        app.get('/college/card', async(req, res)=>{
-            
+        app.get('/colleges/cards', async(req, res)=>{
+            const result = await bookingCollection.find().limit(3).toArray()
+            res.send(result)
+        })
+
+        app.get('/collegeName', async(req, res)=>{
+            const result = await bookingCollection.find().toArray()
+            res.send(result)
+        })
+
+        app.get('/cardDetail/:id', async(req, res)=>{
+            const id = req.params.id
+            const query = {_id: new ObjectId(id)}
+            const result = await bookingCollection.findOne(query)
+            res.send(result)
+        })
+
+
+        app.get('/reviews', async (req, res)=>{
+            const result = await reviewCollection.find().toArray()
+            res.send(result)
         })
 
 
